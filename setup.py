993d4c91bbf7ab236fa32a0a958b6a5c6850216d
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-04-13 02:10:54 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-lavviebot",
+    version="0.0.1",
+    author="Robert Drinovac",
+    author_email="unlisted",
+    description="A Python library for the Purrsong API utilized by LavvieBot S litterboxes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/RobertD502/python-lavviebot',
+    keywords='lavviebot, lavviebot s, purrsong',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ),
+    project_urls={  # Optional
+    'Bug Reports': 'https://github.com/RobertD502/python-lavviebot/issues',
+    'Source': 'https://github.com/RobertD502/python-lavviebot/',
+    },
+)
