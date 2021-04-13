6ed509d05eedbfcbe2859d9d4be033fe958c5019
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-04-12 23:38:42 -0400

    Create lavviebot.py

---
+++ lavviebot/devices/lavviebot.py
@@ -0,0 +1,22 @@
+import time
+import collections
+
+
+class LavvieBot(object):
+    def __init__(self, data, api):
+        self.api = api
+        self.lavviebot_id = data['lavviebotId']
+        self.name = data['lavviebotNickname']
+        self.refresh()
+
+    def refresh(self):
+        lavviebot_status = self.api.lavviebot_status(self)
+        self.waste_status = lavviebot_status['wasteDrawerStatus']
+        self.litter_top_amount = lavviebot_status['litterTopAmount'] / 455.1
+        self.litter_bottom_amount = lavviebot_status['litterBottomAmount'] / 455.1
+        self.litter_type = lavviebot_status['litterType']
+        self.min_bottom_ben_weight = lavviebot_status['minBottomBenWeight'] / 455.1
+        self.min_bottom_nat_weight = lavviebot_status['minBottomNatWeight'] / 455.1
+        self.temperature = lavviebot_status['temperature']
+        self.humidity = lavviebot_status['humidity']
+        self.wait_time = lavviebot_status['waitTime']
