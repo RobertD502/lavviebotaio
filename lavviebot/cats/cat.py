1baa5c45b9fbaa425542baefea291eb7baf8c2e0
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-06-30 16:43:01 -0400

    handle exceptions

--- lavviebot/cats/cat.py
+++ lavviebot/cats/cat.py
@@ -17,23 +17,26 @@ class Cat(object):
         self.refresh()
 
     def refresh(self):
-        cat_status = self.api.cat_status(self)
-        self.cat_attributes = {}
-        if len(cat_status) > 0:
-            cs = cat_status[self.index]
-            if cs.get('catWeight') == None:
-                self.cat_attributes['cat_weight'] = None
-            else:
-                self.cat_attributes['cat_weight'] = cs['catWeight'] / 453592
-            if cs['poopLogs']['dailyPoops'][6].get('newCatWeightAvg') == None:
-                self.cat_attributes['cat_weight_today'] = None
-            else:
-                self.cat_attributes['cat_weight_today'] = cs['poopLogs']['dailyPoops'][6]['newCatWeightAvg'] / 455.1
-            if cs['poopLogs']['dailyPoops'][6].get('poopCount') == None:
-                self.cat_attributes['poop_count'] = None
-            else:
-                self.cat_attributes['poop_count'] = cs['poopLogs']['dailyPoops'][6]['poopCount']
-            if cs['poopLogs']['dailyPoops'][6].get('durationAvg') == None:
-                self.cat_attributes['duration'] = None
-            else:
-                self.cat_attributes['duration'] = cs['poopLogs']['dailyPoops'][6]['durationAvg']
+        try:
+            cat_status = self.api.cat_status(self)
+            self.cat_attributes = {}
+            if len(cat_status) > 0:
+                cs = cat_status[self.index]
+                if cs.get('catWeight') == None:
+                    self.cat_attributes['cat_weight'] = None
+                else:
+                    self.cat_attributes['cat_weight'] = cs['catWeight'] / 453592
+                if cs['poopLogs']['dailyPoops'][6].get('newCatWeightAvg') == None:
+                    self.cat_attributes['cat_weight_today'] = None
+                else:
+                    self.cat_attributes['cat_weight_today'] = cs['poopLogs']['dailyPoops'][6]['newCatWeightAvg'] / 455.1
+                if cs['poopLogs']['dailyPoops'][6].get('poopCount') == None:
+                    self.cat_attributes['poop_count'] = None
+                else:
+                    self.cat_attributes['poop_count'] = cs['poopLogs']['dailyPoops'][6]['poopCount']
+                if cs['poopLogs']['dailyPoops'][6].get('durationAvg') == None:
+                    self.cat_attributes['duration'] = None
+                else:
+                    self.cat_attributes['duration'] = cs['poopLogs']['dailyPoops'][6]['durationAvg']
+        except:
+            return None
