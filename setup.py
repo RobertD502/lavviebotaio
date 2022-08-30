6e4eaf19990237d07e53ad84f3a9fe5d0ae07e31
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2022-08-30 18:59:29 -0400

    bump version to 0.0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lavviebotaio",
-    version="0.0.1",
+    version="0.0.1.1",
     author="Robert Drinovac",
     author_email="unlisted@gmail.com",
     description="Asynchronous Python library for the PurrSong API utilized by LavvieBot S litter boxes",
