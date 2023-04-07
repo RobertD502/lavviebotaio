a4c7f2cfd4e3a51255169a313ef6a52a2356e895
Robert Drinovac <52541649+RobertD502@users.noreply.github.com>
2023-04-07 16:32:45 -0400

    add LB_ERROR_LOG

--- lavviebot/__init__.py
+++ lavviebot/__init__.py
@@ -6,7 +6,7 @@ from lavviebot import model
 from lavviebot.constants import (ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE,
                                  APP_VERSION, BASE_URL, CAT_STATUS, CONNECTION,
                                  CONTENT_TYPE, COOKIE_QUERY, DISCOVER_CATS,
-                                 DISCOVER_LB, LANGUAGE, LB_CAT_LOG, LB_STATUS,
+                                 DISCOVER_LB, LANGUAGE, LB_CAT_LOG, LB_ERROR_LOG, LB_STATUS,
                                  TIMEOUT, TIME_ZONE, TOKEN_QUERY,
                                  UNKNOWN_STATUS, USER_AGENT,)
 from lavviebot.exceptions import (LavviebotAuthError, LavviebotError,)
@@ -16,7 +16,7 @@ from lavviebot.model import (Cat, LavviebotData, LitterBox,)
 __all__ = ['ACCEPT', 'ACCEPT_ENCODING', 'ACCEPT_LANGUAGE', 'APP_VERSION',
            'BASE_URL', 'CAT_STATUS', 'CONNECTION', 'CONTENT_TYPE',
            'COOKIE_QUERY', 'Cat', 'DISCOVER_CATS', 'DISCOVER_LB', 'LANGUAGE',
-           'LB_CAT_LOG', 'LB_STATUS', 'LavviebotAuthError', 'LavviebotClient',
+           'LB_CAT_LOG', 'LB_ERROR_LOG', 'LB_STATUS', 'LavviebotAuthError', 'LavviebotClient',
            'LavviebotData', 'LavviebotError', 'LitterBox', 'TIMEOUT',
            'TIME_ZONE', 'TOKEN_QUERY', 'UNKNOWN_STATUS', 'USER_AGENT',
            'constants', 'exceptions', 'lavviebot_client', 'model']
