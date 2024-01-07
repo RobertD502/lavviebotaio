35def1c202269ed8b352ee549d01d213687cfdf6
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2024-01-07 16:16:00 -0500

    import LavviebotRateLimit exception

--- lavviebot/__init__.py
+++ lavviebot/__init__.py
@@ -9,7 +9,7 @@ from lavviebot.constants import (ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE,
                                  DISCOVER_LB, LANGUAGE, LB_CAT_LOG, LB_ERROR_LOG, LB_STATUS,
                                  TIMEOUT, TIME_ZONE, TOKEN_QUERY,
                                  UNKNOWN_STATUS, USER_AGENT,)
-from lavviebot.exceptions import (LavviebotAuthError, LavviebotError,)
+from lavviebot.exceptions import (LavviebotAuthError, LavviebotError, LavviebotRateLimit,)
 from lavviebot.lavviebot_client import (LavviebotClient,)
 from lavviebot.model import (Cat, LavviebotData, LitterBox,)
 
@@ -17,6 +17,6 @@ __all__ = ['ACCEPT', 'ACCEPT_ENCODING', 'ACCEPT_LANGUAGE', 'APP_VERSION',
            'BASE_URL', 'CAT_STATUS', 'CONNECTION', 'CONTENT_TYPE',
            'COOKIE_QUERY', 'Cat', 'DISCOVER_CATS', 'DISCOVER_LB', 'LANGUAGE',
            'LB_CAT_LOG', 'LB_ERROR_LOG', 'LB_STATUS', 'LavviebotAuthError', 'LavviebotClient',
-           'LavviebotData', 'LavviebotError', 'LitterBox', 'TIMEOUT',
+           'LavviebotData', 'LavviebotError', 'LavviebotRateLimit', 'LitterBox', 'TIMEOUT',
            'TIME_ZONE', 'TOKEN_QUERY', 'UNKNOWN_STATUS', 'USER_AGENT',
            'constants', 'exceptions', 'lavviebot_client', 'model']
