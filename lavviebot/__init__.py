ae9892d01a80f99d7941139f764bf72e5cae83bb
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2024-01-08 14:37:03 -0500

    import LOGGER

--- lavviebot/__init__.py
+++ lavviebot/__init__.py
@@ -10,13 +10,13 @@ from lavviebot.constants import (ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE,
                                  TIMEOUT, TIME_ZONE, TOKEN_QUERY,
                                  UNKNOWN_STATUS, USER_AGENT,)
 from lavviebot.exceptions import (LavviebotAuthError, LavviebotError, LavviebotRateLimit,)
-from lavviebot.lavviebot_client import (LavviebotClient,)
+from lavviebot.lavviebot_client import (LavviebotClient, LOGGER)
 from lavviebot.model import (Cat, LavviebotData, LitterBox,)
 
 __all__ = ['ACCEPT', 'ACCEPT_ENCODING', 'ACCEPT_LANGUAGE', 'APP_VERSION',
            'BASE_URL', 'CAT_STATUS', 'CONNECTION', 'CONTENT_TYPE',
            'COOKIE_QUERY', 'Cat', 'DISCOVER_CATS', 'DISCOVER_LB', 'LANGUAGE',
            'LB_CAT_LOG', 'LB_ERROR_LOG', 'LB_STATUS', 'LavviebotAuthError', 'LavviebotClient',
-           'LavviebotData', 'LavviebotError', 'LavviebotRateLimit', 'LitterBox', 'TIMEOUT',
+           'LavviebotData', 'LavviebotError', 'LavviebotRateLimit', 'LitterBox', 'LOGGER', 'TIMEOUT',
            'TIME_ZONE', 'TOKEN_QUERY', 'UNKNOWN_STATUS', 'USER_AGENT',
            'constants', 'exceptions', 'lavviebot_client', 'model']
