3884adb7b7dabe4c83f1e5f26cc0addc78206cd1
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2025-06-24 13:31:32 -0400

    add AUTO_LOGIN_QUERY

--- lavviebot/__init__.py
+++ lavviebot/__init__.py
@@ -4,7 +4,7 @@ from lavviebot import lavviebot_client
 from lavviebot import model
 
 from lavviebot.constants import (ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE,
-                                 APP_VERSION, BASE_URL, CAT_STATUS, CONNECTION,
+                                 APP_VERSION, AUTO_LOGIN_QUERY, BASE_URL, CAT_STATUS, CONNECTION,
                                  CONTENT_TYPE, COOKIE_QUERY, DISCOVER_CATS,
                                  DISCOVER_DEVICES, LANGUAGE, LAVVIE_SCANNER_STATUS,
                                  LAVVIE_TAG_STATUS, LB_CAT_LOG, LB_ERROR_LOG, LB_STATUS,
@@ -15,7 +15,7 @@ from lavviebot.lavviebot_client import (LavviebotClient, LOGGER)
 from lavviebot.model import (Cat, LavviebotData, LavvieScanner, LavvieTag, LitterBox,)
 
 __all__ = ['ACCEPT', 'ACCEPT_ENCODING', 'ACCEPT_LANGUAGE', 'APP_VERSION',
-           'BASE_URL', 'CAT_STATUS', 'CONNECTION', 'CONTENT_TYPE',
+           'AUTO_LOGIN_QUERY', 'BASE_URL', 'CAT_STATUS', 'CONNECTION', 'CONTENT_TYPE',
            'COOKIE_QUERY', 'Cat', 'DISCOVER_CATS', 'DISCOVER_DEVICES', 'LANGUAGE',
            'LB_CAT_LOG', 'LB_ERROR_LOG', 'LB_STATUS', 'LavviebotAuthError', 'LavviebotClient',
            'LavviebotData', 'LavviebotError', 'LavviebotRateLimit', 'LavvieScanner', 'LAVVIE_SCANNER_STATUS',
